1528fd38bff2d98c9aad45559558dfc289789fbf
alexis diaz <176757434+AlexisDiaz000@users.noreply.github.com>
2025-02-24 17:00:22 -0500

    primera practica

---
+++ practica JS/practica001.js
@@ -0,0 +1,16 @@
+
+let nombre = "alexis"
+let apellido = "diaz"
+let edad = 28;
+let pais = "España"
+let conocimientos = ["java", "git", "github"];
+
+console.log(`hola soy ${nombre} ${apellido} tengo ${edad} soy de ${pais} y tengo estos conocimientos 
+    ${conocimientos[0]}, ${conocimientos[1]}, ${conocimientos[2]}.`);
+
+edad = 30;
+conocimientos.push("javascript")
+
+console.log(`ahora tengo ${edad} años y se un nuevo lenguaje ${conocimientos[3]}`);
+
+
