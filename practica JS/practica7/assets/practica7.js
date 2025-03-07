9ba762c4d6ed0b152815e58bd31aba914f01d3c1
alexis diaz <176757434+AlexisDiaz000@users.noreply.github.com>
2025-03-06 21:55:17 -0500

    practica 7

---
+++ practica JS/practica7/assets/practica7.js
@@ -0,0 +1,13 @@
+// PEDIR FECHA ACTUAL
+const actualYear = new Date().getFullYear();
+
+let year = 1900;
+let result = 0;
+
+do{
+    year = parseInt(prompt('En que año naciste', year))
+}while(year < (actualYear -120) || year > actualYear)
+
+result = actualYear - year;
+
+alert('Tienes ' + result + ' años')
